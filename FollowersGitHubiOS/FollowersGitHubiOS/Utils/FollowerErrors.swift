2b6fea871c787a9fb4a7f44e98a9f15270c18374
Thiago Oliveira <thiago@outlook.com.br>
2022-05-23 17:09:38 -0300

    Adding ui helper and default error message enum

---
+++ FollowersGitHubiOS/FollowersGitHubiOS/Utils/FollowerErrors.swift
@@ -0,0 +1,17 @@
+//
+//  FollowerErrors.swift
+//  FollowersGitHubiOS
+//
+//  Created by Thiago de Oliveira Santos on 23/05/22.
+//
+
+import Foundation
+
+enum FollowerErrors: String, Error {
+    case invalidUsername    = "This username created an invalid request, Please try again."
+    case unableToComplete   = "Unable to complete your request, Please check your internet connection"
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from the server was invalid, Please try again."
+    case unableToFavorite   = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You've already favorited this user. You must REALLY like them!"
+}
---
+++ FollowersGitHubiOS/FollowersGitHubiOS/Utils/FollowersUIHelper.swift
@@ -0,0 +1,24 @@
+//
+//  FollowersUIHelper.swift
+//  FollowersGitHubiOS
+//
+//  Created by Thiago de Oliveira Santos on 23/05/22.
+//
+
+import UIKit
+
+enum FollowersUIHelper {
+    static func createThreeColumnsFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+
+        return flowLayout
+    }
+}
